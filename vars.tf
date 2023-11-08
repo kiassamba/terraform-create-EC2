574610c2173af4be8e4578a6dc75ee7c843d87ab
kiassamba <65406424+kiassamba@users.noreply.github.com>
2023-11-08 12:14:53 +0100

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,1 +1,17 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
 
+
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+
+
+variable "AWS_AMIS" {
+  type =  map
+  default = {
+     "us-east-1" = "ami-0dbc3d7bc646e8516"
+  }
+}
